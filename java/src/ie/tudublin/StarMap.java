022ae9a1e2a8ddc43dd7fa41572a058a8b52e0a7
Alanas Jakubauskas <alanasjakubauskas@gmail.com>
2020-03-09 14:18:43 +0000

    Spinny graph of stars

--- java/src/ie/tudublin/StarMap.java
+++ java/src/ie/tudublin/StarMap.java
@@ -22,6 +22,7 @@ public class StarMap extends PApplet
     {
         loadData();
         printStars();
+        
     }
 
     public void loadData()
@@ -42,9 +43,53 @@ public class StarMap extends PApplet
         }
     }
 
+    int inc = 0;
     public void draw()
     {
+       
+        inc++;
+        resetMatrix();
+        translate(width/2,height/2);
+        scale(.5f);
+        translate((inc*10)%400,0);
+
+        rotate((float)inc/20);
+    
+
+        
+
+        background(0);
+    
+
+        for( int i = -5 ; i <= 5 ; i++){
+
+            
+            stroke(255);
+            float temp = map(i,-5 , 5, height/2, -height/2);
+
+
+
+            text(i+"",-20-width/2,temp);
+            text(-i+"",temp,-20-height/2);
+
+
+            line(temp,height/2,temp,-height/2);
+            line(height/2,temp,-height/2,temp);
+
+        }
+
+        for(Star s : stars){
+
+            stroke(0,255,0);
+            float multiplier = 400/5;
+
+            //-5, 5 to 400 -400
+
+            circle(s.getxG()*multiplier,-s.getyG()*multiplier,10);
+
+        }
 
     }
 
+    // hint sheet on brightspace
 }
